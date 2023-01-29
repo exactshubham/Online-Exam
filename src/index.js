ce9d91e0e70c1bd395de2c9cca04920e73025409
exactshubham <exactshubham@gmail.com>
2023-01-29 19:39:30 +0530

    progress saved

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
+import App from './App';
+import Exam from './exam';
+import Code from './code';
+import Admin from './admin';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}></Route>
+      <Route path="/exam" component={Exam}></Route>
+      <Route path="/code" component={Code}></Route>
+      <Route path="/admin" component={Admin}></Route>
+  </Router>
+), document.getElementById('root'))
